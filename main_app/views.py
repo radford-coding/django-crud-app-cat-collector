98ce564d6eeaf8082c3b20ef8a6d19d70595a41d
Matt Radford <quinquex@gmail.com>
2025-03-31 14:28:28 -0700

    formatting

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render # type: ignore
 
-from django.http import HttpResponse # type: ignore
+# from django.http import HttpResponse # type: ignore
 
 # Create your views here.
 
