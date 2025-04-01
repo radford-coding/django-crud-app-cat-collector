468787905276cd57d79631d0e1d165d1e81a613b
Matt Radford <quinquex@gmail.com>
2025-04-01 09:59:52 -0700

    cat details page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin # type: ignore
+from .models import Cat
 
 # Register your models here.
+
+admin.site.register(Cat)
--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,23 @@
-from django.shortcuts import render # type: ignore
-
+from django.shortcuts import render  # type: ignore
 # from django.http import HttpResponse # type: ignore
+from .models import Cat
 
 # Create your views here.
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
 
-# dummy data - will be replaced by database models later
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6),
-]
 
 def cat_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
