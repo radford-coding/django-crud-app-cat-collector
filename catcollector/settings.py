e86a5218c348aab252fe2989fb4a42da1b9128aa
Matt Radford <quinquex@gmail.com>
2025-04-04 08:24:40 -0700

    log out

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# specify where successful logins go
+# specify where successful logins/logouts go
 LOGIN_REDIRECT_URL = 'cat-index'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
